fa3c07af088cd0f0b8bc4a3d5f182978ff197c50
Griffin Yourick <gryphon92@gmail.com>
2016-04-22 12:07:38 -0400

    Fix symbolMirror export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+export symbolMirror from './symbolMirror.js';
+
 export default function constantMirror(...constants) {
   return constants.reduce((obj, constant) => (
     (constant) ? { ...obj, [constant]: constant } : obj
