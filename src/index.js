ca0b6b12040f84e221aa45afb47f7577d2729800
Griffin Yourick <gryphon92@gmail.com>
2019-05-09 13:32:14 -0400

    Add docs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 export { default as symbolMirror } from './symbolMirror';
 
+/**
+ * @param {...string} constants
+ * @returns {Object<string,string>} an object with mirrored string keys and values.
+ */
 export default function constantMirror(...constants) {
   return constants.reduce(
     (obj, constant) => (constant ? { ...obj, [constant]: constant } : obj),
--- src/symbolMirror.js
+++ src/symbolMirror.js
@@ -1,3 +1,7 @@
+/**
+ * @param {...string} constants
+ * @returns {Object<string,Symbol>} an object with string keys and mirrored symbol values.
+ */
 export default function symbolMirror(...constants) {
   return constants.reduce(
     (obj, constant) => (constant ? { ...obj, [constant]: Symbol.for(constant) } : obj),
